26245889bb80038d82eac64079205fb5f38aae0a
Mendy Wolosow1 <mendywoly+1@gmail.com>
2018-04-20 03:38:40 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,9 +19,5 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types.each do |val|
-    if val.include?(array)
-      return val
-    end
-  end
+  
 end
